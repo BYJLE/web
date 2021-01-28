80831413f2f0fbf3a56c952c8635abd09e863d7f
byj <byj19981225@163.com>
2021-01-28 09:32:51 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  title: "不是傻瓜是光洙",
+  description: "学习",
+  themeConfig: {
+    lastUpdated: '更新时间',
+    nav: [
+      { text: "余光中简介", link: "/introduce" },
+      { text: "语录摘抄", link: "/exteract" },
+      //   {
+      //     text: "Languages",
+      //     items: [
+      //       {
+      //         text: "Group1",
+      //         items: [
+      //           { text: "Home", link: "/" },
+      //           { text: "Guide", link: "/about" },
+      //         ],
+      //       },
+      //     ],
+      //   },
+      { text: "External", link: "https://google.com" },
+    ],
+  },
+};
